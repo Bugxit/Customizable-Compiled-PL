5cb965d3876dce7e496ead541f2c1ab28db26668
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-03-28 20:40:28 +0100

    Update COMBINED.asm

--- AssemblerFunctions/BitwiseOperands/COMBINED.asm
+++ AssemblerFunctions/BitwiseOperands/COMBINED.asm
@@ -37,6 +37,14 @@ section .text
     add rsp, 24
 %endmacro
 
+%macro __macroMov 3
+	push %3
+	push %2
+	push %1
+	call __mov
+    add rsp, 24
+%endmacro
+
 _start:
 	__macroAnd num1, num2, 3
 
@@ -139,6 +147,40 @@ __xorLoop:
 
 	cmp rax, [rsp+32]
 	jge __xorLoop
+
+	pop rcx
+	pop rbx
+	pop rax
+	ret
+global _start
+
+section .data
+num1 dq 0, 4, 3
+num2 dq 0, 5, 5
+
+section .text
+
+__Mov:
+	push rax
+	push rbx
+	push rcx
+	mov rax, [rsp+32]
+	mov rbx, [rsp+40]
+	mov rcx, [rsp+48]
+	dec rcx
+	shl rcx, 3
+	add rax, rcx
+	add rbx, rcx
+
+__movLoop:
+	mov rcx, [rbx]
+	mov [rax], rcx
+
+	sub rax, 8
+	sub rbx, 8
+
+	cmp rax, [rsp+32]
+	jge __andLoop
 
 	pop rcx
 	pop rbx
