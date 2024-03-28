e65610bc17dbb8053de1eba59b3a8ea4b5eae507
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-03-28 20:35:25 +0100

    Update COMBINED.asm

--- AssemblerFunctions/BitwiseOperands/COMBINED.asm
+++ AssemblerFunctions/BitwiseOperands/COMBINED.asm
@@ -5,6 +5,14 @@ num1 dq 0, 4, 3
 num2 dq 0, 5, 5
 
 section .text
+
+%macro __macroNot 2
+	push %2
+	push %1
+	call __not
+    add rsp, 24
+%endmacro
+
 %macro __macroAnd 3
 	push %3
 	push %2
@@ -16,9 +24,30 @@ section .text
 _start:
 	__macroAnd num1, num2, 3
 
-    mov rax, 60
+    	mov rax, 60
 	mov rdi, [num1+16]
 	syscall
+
+__not:
+	push rax
+	push rbx
+	mov rax, [rsp+24]
+	mov rbx, [rsp+32]
+	dec rbx
+	shl rbx, 3
+	add rax, rbx
+
+__notLoop:
+	not qword [rax]
+
+	sub rax, 8
+
+	cmp rax, [rsp+24]
+	jge __notLoop
+
+	pop rbx
+	pop rax
+	ret
 
 __and:
 	push rax
