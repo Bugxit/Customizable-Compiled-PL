d7ec1e6b6e02123eaf9869659702707a3ed5fff9
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-03-28 16:28:50 +0100

    Update xor.asm

--- AssemblerFunctions/xor.asm
+++ AssemblerFunctions/xor.asm
@@ -1,1 +1,37 @@
+global _start
 
+section .data
+num1 dq 0, 0, 0
+num2 dq 0, 0, 7
+
+section .text
+%macro 3
+%endmacro
+_start:
+	push 3
+	push num2
+	push num1
+	jmp __xor
+
+__xor:
+	push rax
+	push rbx
+	push rcx
+	mov rax, [rsp+32]
+	mov rbx, [rsp+40]
+	mov rcx, [rsp+48]
+	shl rcx, 3
+	add rax, rcx
+	add rbx, rcx
+
+__xorLoop:
+	mov rcx, [rbx]
+	xor [rax], rcx
+
+	sub rax, 8
+	sub rbx, 8
+
+	cmp rax, [rsp+32]
+	jge __xorLoop
+
+	
