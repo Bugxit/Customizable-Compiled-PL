5f9db01eb170207ae602830f8c9d368272f795d5
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-04-13 12:01:11 +0200

    Màj

---
+++ oFile.asm
@@ -0,0 +1,16 @@
+global __func__main
+section .text
+__func__main:
+	char i = '/';
+	for(int c = 0;c < 10;c++)
+	{
+	write("%c", i);
+	}
+	return 0;
+	printf("nonononon");
+	mov rax, 60
+	mov rdi, 0
+	syscall
+__func__notmain:
+	return 12;
+	ret
