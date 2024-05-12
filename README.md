d3eb70c4914902251e8a7578ae2aa57c7ba574eb
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-05-12 11:33:57 +0200

    Refactoring and doing great

--- README.md
+++ README.md
@@ -1,23 +1,23 @@
 ## Customizable Programming Language
 ## Summary :
-* [Go](#what-is-cpl-exactly) - What is CPL?
-* [Go](#get-started-with-cpl) - How to get started with CPL?
-* [Go](#write-code-in-cpl) - How to write code in CPL?
-* [Go](#use-intern-features-of-cpl) - How to use intern features of CPL?
-* [Go](#how-does-cpl-work) - How does CPL work?
+* [Go](#what-is-ccpl-exactly) - What is CCPL?
+* [Go](#get-started-with-ccpl) - How to get started with CCPL?
+* [Go](#write-code-in-ccpl) - How to write code in CCPL?
+* [Go](#use-intern-features-of-ccpl) - How to use intern features of CCPL?
+* [Go](#how-does-ccpl-work) - How does CCPL work?
 * [Go](#error-codes) - Error codes...
 
 /!\Please excuse any syntaxical or grammatical error, english is <b>NOT</b> my native language...
-## What is CPL exactly?
-### - What does CPL stands for?
-  As you might have seen in the title, CPL stands for :
+## What is CCPL exactly?
+### - What does CCPL stands for?
+  As you might have seen in the title, CCPL stands for :
 ```
 Customizable Programming Language
 ```
 ### - Why "Customizable"?
-  As the name might suggest, the main attribute of CPL is to be "Customizable".
+  As the name might suggest, the main attribute of CCPL is to be "Customizable".
   <br>However, most of you already asked yourself what "Customizable" means for a programing language?
-  <br>Don't worry, it is as easy as it can get : Every element (grammar, etc) CPL uses can be changed in to whatever you want. For example consider:
+  <br>Don't worry, it is as easy as it can get : Every element (grammar, etc) CCPL uses can be changed in to whatever you want. For example consider:
   ```
 write ("Hello, world!");
   ```
@@ -33,26 +33,26 @@ write  ->  dontwrite
 ```
 
 ### - What if my friend and I don't use the same settings?
-  When learning about the customizable feature of CPL you probably thought it could bring some 
+  When learning about the customizable feature of CCPL you probably thought it could bring some 
   <br>issues. (We all have that one friend who would like to use the weirdest grammar possible)
-  <br>To address this issue, there exists a feature which automaticaly changes the grammar in a CPL file to your own grammar.
-  <br>In case you want to learn more about doing this : please follow [this link](#use-intern-features-of-cpl)
+  <br>To address this issue, there exists a feature which automaticaly changes the grammar in a CCPL file to your own grammar.
+  <br>In case you want to learn more about doing this : please follow [this link](#use-intern-features-of-CCPL)
 
-### - Is CPL compiled?
-  CPL is a language compiled in NASM for the x86-64 CPU family, however this temporary and
-  <br>CPL will soon support more CPUs. 
+### - Is CCPL compiled?
+  CCPL is a language compiled in NASM for the x86-64 CPU family, however this temporary and
+  <br>CCPL will soon support more CPUs. 
 
-### - Who is working on the CPL project?
-  Sadly, I (aka Bugxit) am working alone on the CPL project, which explains the lack of updates and new features.
+### - Who is working on the CCPL project?
+  Sadly, I (aka Bugxit) am working alone on the CCPL project, which explains the lack of updates and new features.
   <br>But, being alone doesn't mean you can't produce things as good as a team (or does it?). 
   <br>I am still taking this project seriously and I am trying my best to make this PL the best it can be!
 
-### - What are the goals of CPL?
-  Since CPL is compiled it is an extremely fast language, further more it aims at allowing a faster approach 
+### - What are the goals of CCPL?
+  Since CCPL is compiled it is an extremely fast language, further more it aims at allowing a faster approach 
   <br>to the Bignums used in Python & Javascript.
 
-## Get started with CPL!
-## Write code in CPL:
+## Get started with CCPL!
+## Write code in CCPL:
 ### The Keywords :
 #### Summary:
 - [Go](#write) - Write
@@ -67,7 +67,7 @@ write  ->  dontwrite
 - [Go](#boolean-object) - Boolean Object
 #### Key informations:
 
-## Use intern features of CPL: 
-## How does CPL work?
+## Use intern features of CCPL: 
+## How does CCPL work?
 ## Error codes:
 
--- compiler.py
+++ compiler.py
@@ -1,156 +1,52 @@
-def main(sFile, oFile):
-    with open(sFile, 'r') as file: tempFileArray = file.readlines()
+from sys import argv
+from os import path
 
-    sFileArray = []
-    skipChar = False
-    strOpen = False
-    commOpen = False
-    strOpenChar = ""
-    curLine = ""
-    for line in tempFileArray:
-        line = line.strip()
-        for charPos, char in enumerate(line):
-            if skipChar:
-                skipChar = False 
-                continue
-            if not strOpen and not commOpen and char in ["'", '"']:
-                strOpenChar = char
-                strOpen = True
-            elif strOpen and not commOpen and char == strOpenChar: strOpen = False
-
-            if charPos != len(line) - 1:
-                if not commOpen and not strOpen and f'{char}{line[charPos+1]}' == '//': break
-                elif not commOpen and not strOpen and f'{char}{line[charPos+1]}' == '/*': commOpen, skipChar = True, True
-                elif commOpen and not strOpen and f'{char}{line[charPos+1]}' == '*/': commOpen, skipChar = False, True
-
-            if not strOpen and not commOpen and char in ['{', '}']:
-                if curLine.strip() != "": sFileArray.append(curLine)
-                sFileArray.append(char)
-                curLine = ""
-                continue
-
-            if not strOpen and not commOpen and char == ";" and curLine[:3] != "for":
-                if curLine.strip() != "": sFileArray.append(f'{curLine}{char}')
-                curLine = ""
-                continue
-
-            if not commOpen: curLine += char        
-    funcDict = {}
-    globalVars = {}
-    forbidLines = []
-
-    for lineNumber, line in enumerate(sFileArray):
-        if lineNumber in forbidLines: continue
-        if line[:5] == "func ":                
-            lineStr = remStr(line)
-            opBrack = 0
-            brackAlrdOpen = False
-            funcName, funcArgs, argsFn = "", "", -1
-            for cN, char in enumerate(lineStr[5:]):
-                if char == '(': opBrack, brackAlrdOpen = opBrack + 1, True
-                elif char == ')': opBrack += -1
-                if not brackAlrdOpen: funcName += line[cN+5]
-                else: funcArgs += line[cN+5]
-                if opBrack == 0 and brackAlrdOpen: 
-                    argsFn = cN+5
-                    break
-            if opBrack != 0 or not brackAlrdOpen: exit(1)
-            funcName = funcName.strip()
-            if opBrack != 0 or '(' not in lineStr[5:]: exit(1)
-
-            funcType = line[argsFn+1:].strip()
-            if funcType[:2] != "->": exit(1)
-            funcType = funcType[2:].strip()
-
-            funcArgs = [funcArgs]
-            while ',' in remStr(funcArgs[-1]):
-                remArgs = remStr(funcArgs[-1])
-                funcArgs.append(funcArgs[-1][remArgs.index(",")+1:])
-                funcArgs[-2] = funcArgs[-2][:remArgs.index(",")]
-
-            if funcName in list(funcDict.keys()) and (funcType != funcDict[funcName]["type"] or funcArgs != funcDict[funcName]["args"]): 
-                print("error")
-                exit(1)
-            elif funcName not in list(funcDict.keys()): funcDict[funcName] = {'type' : funcType, 'args' : funcArgs, 'lines' : []}
-            opBrack = 1
-            forbidLines.append(lineNumber+1)
-            for lN, l in enumerate(sFileArray[lineNumber+2:]):
-                forbidLines.append(lN+lineNumber+2)
-                if l == '{':  opBrack += 1
-                elif l == '}': opBrack += -1
-                if opBrack == 0: break
-                funcDict[funcName]['lines'].append(l)
-        else:
-            pass
-            """
-            varType = line.split(' ', 1)[0]
-            varName = line[:-1].split(' ')[-1]
-            if varName in list(globalVars.keys()):
-                print("error")
-                exit(1)
-            globalVars[varName] = {"type": varType}
-            """
-
-    if "main" not in list(funcDict.keys()): 
-        print("error")
-        exit(1)
-
-    print(funcDict)
-
-    newArray = []
-    newArray.append("global __main__")
-    newArray.append("section .data")
-    for varName in list(globalVars.keys()):
-        newArray.append(f'\t{varName} {globalVars[varName]["type"]}')
-    newArray.append("section .text")
-    for funcName in list(funcDict.keys()):
-        newArray.append(f'__{funcName}__:')
-        for line in evalLines(funcDict[funcName]['lines'], f'__{funcName}__'): newArray.append(line)
-        if funcName != "main": newArray.append(f'\tret')
-        else: 
-            newArray.append("\tmov rax, 60")
-            newArray.append("\tmov rdi, [rsp+8]")
-            newArray.append("\tsyscall")
-
-    sFileArray = newArray
-
-    with open(oFile, 'w') as file: 
-        for line in sFileArray: file.write(f'{line}\n')
-
-def evalLines(oArray : list, stateName : str) -> list[str]:
-    retArray = []
-    forNumber = 0
-    forbidLines = []
+def getInputFile(filePath : str) -> list[str]:
+    with open(filePath, "r") as inputFile: fileArray =  inputFile.readlines()
     
-    for lN, line in enumerate(oArray): 
-        if line[:3] == "for":
-            forArgs = line.split('(', 1)[1].rsplit(')', 1)[0]
-            forArgs = [forArgs[:remStr(forArgs).index(';')+1].strip(), forArgs[remStr(forArgs).index(';')+1:]]
-            forArgs = [forArgs[0], forArgs[1][:remStr(forArgs[1]).index(';')+1].strip(), forArgs[1][remStr(forArgs[1]).index(';')+1:]]
-            retArray.append(f'\t{forArgs[0]}')
-            retArray.append(f'{stateName}for__{forNumber}:')
-            retArray.append(f'\t{forArgs[1]}')
-            forNumber += 1
-            for line in oArray[lN:]: pass #Check lines in for loop
-        else: retArray.append(f'\t{line}')
+    if fileArray == -1: print("An error occured while reading the file") ; exit(1)
 
-    return retArray
+    return [line.strip() for line in fileArray if line.strip()]
 
-def remStr(oriStr) -> str:
-    retStr = ""
-    strOpen = False
-    strOpenChar = ""
-    for char in oriStr:
-        if not strOpen and char in ["'", '"']:
-            strOpenChar, strOpen = char, True
-            retStr += char
-            continue
-        elif strOpen and char == strOpenChar: strOpen = False
+def splitFileSettings(fileArray : list[str]) -> list[str]:
+    settingsArr = []
+    remainderArr = []
+    for line in fileArray: 
+        if line[0] == '#': settingsArr.append(line)
+        else: remainderArr.append(line)
 
-        if strOpen: retStr += " "
-        else: retStr += char
+    return settingsArr, remainderArr
 
-    return retStr
+def separateClasses(codeArray: list[str]) -> list[str]:
+    classesList = []
+    remainderArr = []
+    inClassesLines = []
+    for lineNumber, line in enumerate(codeArray):
+        if lineNumber in inClassesLines: continue
+        if line[:5].lower() == "class":
+            classesList.append([])
+            classClosed = False
+            for classLineNumber, classLine in enumerate(codeArray[lineNumber:]):
+                inClassesLines.append(lineNumber + classLineNumber)
+                classesList[-1].append(classLine)
+                if classLine[:8].lower() == "endclass": classClosed = True ; break
+            if not classClosed: print("Classe did not end before file") ; exit(1)
+        else: remainderArr.append(line)
+
+    return classesList, remainderArr
 
 if __name__ == "__main__":
-    main('file.cpl', 'oFile.asm')
+    if len(argv) < 3: print("Not enough inputs!") ; exit(1)
+    argv = argv[1:]
+    if not path.isfile(argv[0]): print("Input file does not exist") ; exit(1)
+    if path.isfile(argv[1]): 
+        if input(f"Are you sure you want to delete {argv[1]}? [Y/N]\n").lower() != "y": exit(1)
+        
+    inputFileArray = getInputFile(argv[0])
+
+    settingsArray, codeFileArray = splitFileSettings(inputFileArray)
+    del inputFileArray
+
+    classesArray, codeFileArray = separateClasses(codeFileArray)
+
+    print(settingsArray, classesArray, codeFileArray, sep=" - ")
