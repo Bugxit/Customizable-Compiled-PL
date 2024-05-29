485855418501767230cd14655ba7dc1df8ce220e
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-05-29 12:38:52 +0200

    Update RustCompiler.rs

--- RustCompiler.rs
+++ RustCompiler.rs
@@ -7,67 +7,77 @@ fn main(){
 
     if argv.len() < 2 { panic!("Please enter a file name!"); }
 
-    let (mut file_vector, file_line_number_vector) : (Vec<String>, Vec<usize>);
+    let file_vector : Vec<String> = read_file(&argv[1]);
+    let file_vector : Vec<(String, usize)> = remove_comments(file_vector);
+    let (file_vector, classes_vector)   : (Vec<(String, usize)>, Vec<Vec<(String, usize)>>) = split_blocks_from_file(file_vector, "class");
+    let (file_vector, structs_vector)   : (Vec<(String, usize)>, Vec<Vec<(String, usize)>>) = split_blocks_from_file(file_vector, "struct");
+    let (file_vector, functions_vector) : (Vec<(String, usize)>, Vec<Vec<(String, usize)>>) = split_blocks_from_file(file_vector, "func");
 
-    file_vector = read_file(&argv[1]);
 
-    (file_vector, file_line_number_vector) = remove_comments(file_vector);
-
-    let thing = split_classes_from_file(file_vector, file_line_number_vector);
-
-    for vec_line in thing
-    {
-        println!("#NEWCLASS:");
-
-        for line in vec_line
-        {
-            println!("{line}");
-        }
-    }
-
-    //for (line_n, line) in file_vector.iter().enumerate() { println!("{} - {}", file_line_number_vector[line_n] ,line); }
+    //Debug: print content of vars
+    for vec_line in classes_vector   { println!("#NEWCLASS:"); for line in vec_line { let (line_value, line_number) = line; println!("{line_number} - {line_value}"); } }
+    for vec_line in structs_vector   { println!("#NEWSTRUCT:"); for line in vec_line { let (line_value, line_number) = line; println!("{line_number} - {line_value}"); } }
+    for vec_line in functions_vector { println!("#NEWFUNC:"); for line in vec_line { let (line_value, line_number) = line; println!("{line_number} - {line_value}"); } }
+    println!("#MAINFILE:"); for line in file_vector { let (line_value, line_number) = line; println!("{line_number} - {line_value}"); }
 }
 
-fn split_classes_from_file(file_vector : Vec<String>, _file_line_number_vector : Vec<usize>) -> Vec<Vec<String>>{
+fn split_blocks_from_file(file_vector : Vec<(String, usize)>, blocks_name : &str) -> (Vec<(String, usize)>, Vec<Vec<(String, usize)>>){
 
-    let mut all_classes_vector : Vec<Vec<String>> = vec![];
+    let block_len : usize = blocks_name.len() + 1;
+
+    let mut all_blocks_vector : Vec<Vec<(String, usize)>> = vec![];
+
+    let mut new_file_vector : Vec<(String, usize)> = vec![];
 
     let mut forbiden_lines : Vec<usize> = vec![];
 
-    'in_file: for (line_number, line) in file_vector.iter().enumerate() 
+    'in_file: for line_info in file_vector.iter()
     {
-        let start_of_line : &str;
-        if line.len() < 5
-        { 
-            forbiden_lines.push(line_number);
-            continue 'in_file; 
-        }
-        start_of_line = &line[..6].trim();
-        if start_of_line != "class" || forbiden_lines.contains(&line_number)
-        { 
-            forbiden_lines.push(line_number);
-            continue 'in_file; 
-        }
+        let (line, line_number) : (String, usize) = line_info.clone();
 
-        let mut new_class_vector : Vec<String> = vec![];
-
-        'in_class: for (line_number_in_class, line_in_class) in file_vector.iter().enumerate()
+        if forbiden_lines.contains(&line_number)
         {
-            if forbiden_lines.contains(&line_number_in_class) { continue 'in_class; }
+            forbiden_lines.push(line_number);
+            continue 'in_file; 
+        }
 
-            forbiden_lines.push(line_number_in_class);
-            new_class_vector.push(String::from(line_in_class));
+        let start_of_line : &str;
+        if line.len() < block_len { start_of_line = &line; }
+        else { start_of_line = &line[..block_len].trim(); }
+        if start_of_line != blocks_name
+        { 
+            new_file_vector.push((line, line_number));
+            forbiden_lines.push(line_number);
+            continue 'in_file; 
+        }
+
+        let mut block_closed_before_file : bool = false;
+        let mut new_block_vector : Vec<(String, usize)> = vec![];
+
+        'in_block: for line_info_in_block in file_vector.iter()
+        {
+            let (line_in_block, line_number_in_block) : (String, usize) = line_info_in_block.clone();
+
+            if forbiden_lines.contains(&line_number_in_block) { continue 'in_block; }
+
+            forbiden_lines.push(line_number_in_block);
+            new_block_vector.push((String::from(line_in_block.clone()), line_number_in_block));
 
             let start_of_line : &str;
-            if line_in_class.len() < 9 { start_of_line = line_in_class; }
-            else { start_of_line = &line_in_class[..9].trim(); }
-            if start_of_line == "endclass" { break 'in_class; }
+            if line_in_block.len() < block_len + 3 { start_of_line = &line_in_block; }
+            else { start_of_line = &line_in_block[..block_len + 3].trim(); }
+            if start_of_line == "end".to_owned() + blocks_name { block_closed_before_file = true; break 'in_block; }
         } 
 
-        all_classes_vector.push(new_class_vector);
+        if !block_closed_before_file{
+            //Error: Reached EOF before "end{block}"
+            panic!("Reached EOF while in a {}", blocks_name);
+        }
+
+        all_blocks_vector.push(new_block_vector);
     }
 
-    all_classes_vector
+    (new_file_vector, all_blocks_vector)
 }
 
 
@@ -80,9 +90,8 @@ fn read_file(file_path : &str) -> Vec<String>{
     file_content
 }
 
-fn remove_comments(file_vector : Vec<String>) -> (Vec<String>, Vec<usize>){
-    let mut new_file_vector : Vec<String> = vec![];
-    let mut new_line_number_vector : Vec<usize> = vec![];
+fn remove_comments(file_vector : Vec<String>) -> Vec<(String, usize)>{
+    let mut new_file_vector : Vec<(String, usize)> = vec![];
 
     let mut ignore_next_char : bool;
     let mut is_string_opened : bool = false;
@@ -124,12 +133,11 @@ fn remove_comments(file_vector : Vec<String>) -> (Vec<String>, Vec<usize>){
         current_line = String::from(current_line.trim());
 
         if !current_line.is_empty() {
-            new_file_vector.push(current_line);
-            new_line_number_vector.push(line_number+1);
+            new_file_vector.push((current_line, line_number+1));
         }
 
         current_line = String::from("");
     }
 
-    (new_file_vector, new_line_number_vector)
+    new_file_vector
 }
