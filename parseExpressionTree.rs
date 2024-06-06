2b442f773bc962ea81c5f62909635df0c7c280a3
Bugxit <101186137+Bugxit@users.noreply.github.com>
2024-06-06 09:41:15 +0200

    Update parseExpressionTree.rs

--- parseExpressionTree.rs
+++ parseExpressionTree.rs
@@ -1,6 +1,5 @@
 
 //Easier? Than importing it =)
-
 #[derive(PartialEq)]
 enum Option<T>
 {
@@ -12,7 +11,7 @@ enum Option<T>
 
 struct ExpressionTree
 {
-    val_type : &str,
+    val_type : String,
     function : String,
     children : Vec<ExpressionTree>
 }
@@ -20,25 +19,62 @@ struct ExpressionTree
 impl ExpressionTree
 {
 
-fn __init__(&mut self, expression : String, separator_vector: Vec<String>) -> ()
+fn __init__(&mut self, expression : String, separator_vector: Vec<String>, depth : i16) -> ()
 {
-    let ( new_function, split_position, separator_vector )
+    let ( expression, split_position, separator_vector )
     = parse_once(expression, separator_vector);
+    
+    if split_position != Option::None
+    {
+        let Option::Some(split_position) = split_position else { return; }; 
+        self.val_type = String::from("operand");
+        self.function = if expression.len() > split_position { String::from(expression.chars().nth(split_position).unwrap()) } else { String::new() }; //Debug: Empty sometimes?!
+        self.children.push( ExpressionTree { val_type : String::new(), function : String::new(), children: Vec::<ExpressionTree>::new() } );
+        self.children.push( ExpressionTree { val_type : String::new(), function : String::new(), children: Vec::<ExpressionTree>::new() } );
+        self.children[0].__init__(String::from(&expression[..split_position]), separator_vector.clone(), depth+1);
+        self.children[1].__init__(String::from(&expression[split_position+1..]), separator_vector.clone(), depth+1);
+        return;
+    }
 
-    self.function = new_function;
-    let Option::Some(a) = split_position else { 
-                                                self.children.push( ExpressionTree { val_type : "", function: String::from(""), children: Vec::<ExpressionTree>::new() } )
-                                                self.children.push( ExpressionTree { val_type : "", function: String::from(""), children: Vec::<ExpressionTree>::new() } )
-                                                self.children[0].__init__(expression[..1], separator_vector.copy())
-                                                self.children[1].__init__(expression[])
-                                                return; };
 
-    self.children.push( ExpressionTree { })
+    self.function = expression;
+}
 
+fn height(&self, depth : i16) -> i16
+{
+    if self.children.len() == 0 { return depth; }
+    let mut max_depth = depth;
+    for child in &self.children 
+    {
+        let new_depth = child.height( depth+1 );
+        max_depth = if new_depth > max_depth { new_depth } else { max_depth };
+    }
+
+    max_depth
+}
+
+fn len(&self) -> usize
+{
+    let mut size = self.function.len() + 2;
+    for child in self.children.iter()
+    {
+        size += child.len();
+    }
+
+    size
+}
+
+fn show(&self, depth : i16) -> ()
+{
+    if depth > 0 { for child in &self.children { child.show(depth-1); } }
+    if depth == 1 { println!(""); }
+    if depth > 0 { return; }
+    for _ in 0..( (self.len() - self.function.len() - 2) / 2) { print!(" "); }
+    print!("{}", self.function);
+    for _ in ( (self.len() - self.function.len() - 2) / 2)..self.len() { print!(" "); } 
 }
 
 }
-
 
 
 fn parse_once(expression : String, mut separator_vector: Vec<String>) -> (String, Option<usize>, Vec<String>)
@@ -49,7 +85,7 @@ fn parse_once(expression : String, mut separator_vector: Vec<String>) -> (String
         let mut is_string_opened : bool = false;
         let mut is_parentheses_opened : bool = false;
         let mut char_that_opened_string : char = ' ';
-        let mut opened_parentheses_count : i16 = 0;
+        let mut opened_parentheses_count : i16 = 0; //Also check for { & [
 
         for (char_number, char_value) in expression.chars().enumerate()
         {
@@ -76,19 +112,25 @@ fn parse_once(expression : String, mut separator_vector: Vec<String>) -> (String
 
             if !is_parentheses_opened && !is_string_opened && separator_vector[0].contains(char_value)
             {
-                return (String::from(char_value), Option::Some(char_number), separator_vector);
+                return (expression, Option::Some(char_number), separator_vector);
             }
         }
 
         separator_vector.remove(0);
     }            
 
-    return (expression, Option::None, separator_vector) //DEBUG 0 should be changed (Option enum)
+    (expression, Option::None, separator_vector)
 }
 
 fn main()
 {
-    let mut root : ExpressionTree = ExpressionTree { val_type : "", function: String::from(""), children: Vec::<ExpressionTree>::new() };
+    let mut root : ExpressionTree = ExpressionTree { val_type : String::new(), function: String::new()
+        , children: Vec::<ExpressionTree>::new() };
 
-    root.__init__(String::from("1+1"), vec![String::from("^&|"), String::from("+-"), String::from("*/%")]);
+    root.__init__(String::from("3*2-1+5/8"), vec![String::from("^&|"), String::from("+-"), String::from("*/%")], 0);
+
+    println!("{} - {}", root.len(), root.height(1));
+    root.show(0);
+    println!("");
+    for i in 1..root.height(1) { root.show(i); }
 }
